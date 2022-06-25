d8b044485a92269ae422b1cf0c9536e3eeaffb03
Chris Heald <104562333+chris-heald-sotf@users.noreply.github.com>
2022-06-25 12:02:24 +0100

    Baseline application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from '@mui/material/Button'
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+      <Button variant="contained">Refresh</Button>
+    </div>
+  );
+}
+
+export default App;
