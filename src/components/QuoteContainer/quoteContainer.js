74944f7b435feec1b760d747e7963c7ccba07511
HumbertoMCC <h.murcia.cuellar@accenture.com>
2022-07-04 11:51:29 +0100

    New quote

--- src/components/QuoteContainer/quoteContainer.js
+++ src/components/QuoteContainer/quoteContainer.js
@@ -1,17 +1,23 @@
 const phrases = [
-    {
-        content: "We cannot solve problems with the kind of thinking we employed when we came up with them.",
-        author: "Albert Einstein"
-    },
-    {
-        content: "When you change your thoughts, remember to also change your world.",
-        author: "Norman Vincent Peale"
-    },
-    {
-        content: "Don't let yesterday take up too much of today.",
-        author: "Will Rogers"
-    }
-]
+  {
+    content:
+      "We cannot solve problems with the kind of thinking we employed when we came up with them.",
+    author: "Albert Einstein",
+  },
+  {
+    content:
+      "When you change your thoughts, remember to also change your world.",
+    author: "Norman Vincent Peale",
+  },
+  {
+    content: "Don't let yesterday take up too much of today.",
+    author: "Will Rogers",
+  },
+  {
+    content: "Luke, I am your father",
+    author: "Darth Vader",
+  },
+];
 
 const getRandomPhrase = (oldPhrase = null) => {
     let newPhrase = phrases[Math.floor(Math.random() * phrases.length)]
